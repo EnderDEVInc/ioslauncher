fb38d2ddd43aad24d9c61252a8f418938e122ee7
EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>
2025-10-06 09:57:47 +0700

    Update LauncherProfilesViewController.m
    Signed-off-by: EnderMinecraft <88361566+EnderMinecraft@users.noreply.github.com>

--- Natives/LauncherProfilesViewController.m
+++ Natives/LauncherProfilesViewController.m
@@ -150,11 +150,11 @@ typedef NS_ENUM(NSUInteger, LauncherProfilesTableSection) {
 }
 
 - (void)setupInstanceCell:(UITableViewCell *) cell atRow:(NSInteger)row {
-    cell.userInteractionEnabled = !getenv("DEMO_LOCK");
+    cell.userInteractionEnabled = 1;
     if (row == 0) {
         cell.imageView.image = [UIImage systemImageNamed:@"folder"];
         cell.textLabel.text = localize(@"preference.title.game_directory", nil);
-        cell.detailTextLabel.text = getenv("DEMO_LOCK") ? @".demo" : getPrefObject(@"general.game_directory");
+        cell.detailTextLabel.text = 0 ? @".demo" : getPrefObject(@"general.game_directory");
     } else {
         NSString *imageName;
         if (@available(iOS 15.0, *)) {
